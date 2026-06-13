ed49ee51a4dee3b79b28731435ec0b7d7e5b11dd
Zulal Aybek <zulalaybek@Zulal-MacBook-Air-2.local>
2026-06-13 14:35:38 +0200

    Correction erreur variable item dans PaletteSection

--- src/app/components/ProjectKh.tsx
+++ src/app/components/ProjectKh.tsx
@@ -463,7 +463,7 @@ function PaletteSection() {
                         />
                         {/* Unique shape per card */}
                         <div className="relative z-10">
-                          <item.icon size={48} className="text-white/60 drop-shadow-[0_0_20px_rgba(255,255,255,0.4)]" />
+                          <card.icon size={48} className="text-white/60 drop-shadow-[0_0_20px_rgba(255,255,255,0.4)]" />
                         </div>
                     </div>
                   </div>
